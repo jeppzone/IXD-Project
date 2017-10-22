fc3df2dfdb26bb6ca2c36e4342f8e831736dbc07
Felixfranzen <felixfranzn@gmail.com>
2017-10-22 19:29:28 +0200

    stats

--- client/index.js
+++ client/index.js
@@ -8,6 +8,9 @@ how many animals lost their home
 
 var API_URL = 'https://ixd-project-backend.herokuapp.com'
 var forest = document.querySelector('#forest .content');
+var hamburgers = document.querySelector('#hamburgers-collected');
+var goneRainforest = document.querySelector('#rainforest-percent');
+var goneAnimals = document.querySelector('#animal-count');
 
 function getRandomInt(min, max) {
     return Math.random() * (max - min + 1) + min;
@@ -38,9 +41,33 @@ function append(el){
   forest.appendChild(el)
 }
 
-function fetch(){
-  return axios.get(API_URL + '/board')
+function setHamburgers(number){
+  hamburgers.innerHTML = number
 }
+
+function setForest(forest){
+  var percent = forest + '%'
+  goneRainforest.style.width = percent
+}
+
+function setAnimals(animals){
+  goneAnimals.innerHTML = animals
+}
+
+function fetch(){
+  return Promise.all([
+      axios.get(API_URL + '/board'),
+      axios.get(API_URL + '/information')
+    ])
+}
+
+// ------ TODO REMOVE -------
+var informationMock = {
+  hamburgers: 1,
+  rainforestGone: 0.1,
+  animalsGone: 1
+}
+// --------------------------
 
 var fetching = false
 var interval = setInterval(function(){
@@ -48,13 +75,29 @@ var interval = setInterval(function(){
     return
   }
   fetching = true
-  fetch().then(function(res){
+  fetch().then(function(response){
+    fetching = false;
+
+    // ------ TODO REMOVE -------
+    informationMock.hamburgers = informationMock.hamburgers + 15
+    informationMock.rainforestGone = informationMock.rainforestGone + 0.05
+    informationMock.animalsGone = informationMock.animalsGone + 1
+    // --------------------------
+
+    var board = response[0].data
+    var information = informationMock //response[1].data
+
+    //generate board
     forest.innerHTML = ''
-    var arr = res.data.board
-    arr.forEach(function(val){
+    board.board.forEach(function(val){
       append(createTile(val))
     })
-    fetching = false
+
+    //generate information
+    setHamburgers(information.hamburgers)
+    setForest(information.rainforestGone * 100)
+    setAnimals(information.animalsGone)
+
   })
 
 }, 5000)
